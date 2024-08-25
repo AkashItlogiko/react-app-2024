34b2ec8860a1b811fb3d08a6becc4b2c939dc349
AkashItlogiko <akashsahanwu089@gmail.com>
2024-08-25 20:13:05 +0600

    First commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import Card from './components/Card';
+import Data from './data.json';
+
+function App() {
+  return (
+    <div>
+      <h1 className="headingStyle">Todo App</h1>
+      {Data.map((item, index) => (
+        <Card key={index} titleText={item.title} descText={item.desc} />
+      ))}
+    </div>
+  );
+}
+export default App;
