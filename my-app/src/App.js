be77757229148547904329c42b1ef576a920992e
AkashItlogiko <akashsahanwu089@gmail.com>
2024-08-27 21:21:33 +0600

    Done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,34 @@
-import Card from './components/Card';
-import Data from './data.json';
+// import List from './components/UniqueList/List';
 
-function App() {
+const users = [
+  {
+    fullName: 'Akash saha',
+    age: 26,
+    phones: [{ home: '017458744' }, { office: '014458744' }],
+  },
+  {
+    fullName: 'Riya Goush',
+    age: 22,
+    phones: [{ home: '0174585460' }, { office: '0150987442' }],
+  },
+];
+
+export default function App() {
   return (
     <div>
-      <h1 className="headingStyle">Todo App</h1>
-      {Data.map((item, index) => (
-        <Card key={index} titleText={item.title} descText={item.desc} />
+      <h1>Nasted Lists</h1>
+      {users.map((user, index) => (
+        <article key={index}>
+          <h3>{user.fullName}</h3>
+          <p>{user.age}</p>
+          {user.phones.map((phone, index) => (
+            <div key={index}>
+              <p>{phone.home}</p>
+              <p>{phone.office}</p>
+            </div>
+          ))}
+        </article>
       ))}
     </div>
   );
 }
-export default App;
